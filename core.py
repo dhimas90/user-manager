5454a8fd9a62080d71966357edc333e9047c0a97
DhimasHernandi <58141958+dhimas90@users.noreply.github.com>
2023-06-19 12:45:06 +0700

    Update core.py

--- core.py
+++ core.py
@@ -6,6 +6,7 @@ from passlib.hash import pbkdf2_sha256
 import re
 load_dotenv()
 import os
+from database import dbuser
 
 app = Flask(__name__)
 
@@ -16,7 +17,16 @@ app.config['MYSQL_DB'] = 'kasir'
  
 mysql = MySQL(app)
 
+# coba disini
+@app.route('/check')
+def access():
+    cur = dbuser.conn
+    cursor = cur.connection.cursor()
+    cursor.execute("SELECT * FROM user")
+    data = cursor.fetchall()
+    return jsonify()
 
+# coba selesai
 @app.route('/user')
 def user():
     cursor = mysql.connection.cursor()
@@ -60,8 +70,46 @@ def usercreate():
         message = 'gagal'
     return message
 
+@app.route('/user/put/<user_id>', methods=['GET', 'POST'])
+def edit(user_id):
+    if request.method == 'GET':
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM user WHERE user_id = %s", (user_id,))
+        resultset = cursor.fetchone()
+        return jsonify(
+            user_id = resultset[0],
+            nama = resultset[1],
+            email = resultset[2],
+            user_pass = resultset[3],
+            level = resultset[4],
+            nationality_id = resultset[5],
+            address = resultset[6],
+            phone = resultset[7]
+        )
+    else :
+        user_id = request.form['user_id']
+        email = request.form['email']
+        name = request.form['name']
+        user_pass = request.form['user_pass']
+        level = request.form['level']
+        nationality_id = request.form['nationality_id']
+        address = request.form['address']
+        phone = request.form['phone']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("UPDATE user SET name = %s, email = %s, user_pass = %s, nationality_id = %s, address = %s, phone = %s WHERE user_id = %s", (name, email, user_pass, nationality_id, address,phone, level, user_id, ))
+        action = mysql.connection.commit()
+        return jsonify(action)
 
 
+        # cursor = mysql.connection.cursor()
+        # cursor.execute("UPDATE user SET nama = ")
+
+# @app.route("/user/edit", methods='PUT')
+# def editpost():
+#     cursor = mysql.connection.cursor()
+#     request.get_json()
+#     cursor.execute("UPDATE user SET email = ")
 
 
         
